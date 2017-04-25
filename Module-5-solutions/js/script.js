ecfe0be282d5fe9189d81e86b78d4170d2633848
taru82 <upadhyay.taru1/2gmail.com>
2017-04-24 22:27:43 -0400

    fix view special

--- Module-5-solutions/js/script.js
+++ Module-5-solutions/js/script.js
@@ -119,8 +119,8 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                 // it into the home html snippet.
 
                 // var homeHtmlToInsertIntoMainPage = buildCategoriesViewHtml(categories, homeHtml, chosenCategoryShortName);
-                        var homeHtmlToInsertIntoMainPage = buildRandomCategory(categoryHtml, homeHtml, chosenCategoryShortName);
-                        console.log('chosenCategoryShortName' + chosenCategoryShortName);
+				var homeHtmlToInsertIntoMainPage = insertProperty( homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+                        console.log('homeHtmlToInsertIntoMainPage' + homeHtmlToInsertIntoMainPage);
 
                 // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
